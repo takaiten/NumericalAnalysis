d4087d2fceaccb50318ee63d676705ddb13adb64
Артем <a.tatarincev.2017@azure.nstu.ru>
2019-09-29 21:17:55 +0800

    added the creation of a random matrix for measuring time

--- NumericalAnalysis/Program.cs
+++ NumericalAnalysis/Program.cs
@@ -12,21 +12,31 @@ namespace ComMethods
             {
                 var T = new Thread(() =>
                 {
-                    const int size = 20;
+                    const int size = 500;
                     Matrix A = new Matrix(size, size);
-                    Vector xTrue = new Vector(size);
+
+                    Vector F = new Vector(size);
+                    Random device = new Random();
                     for (int i = 0; i < size; i++)
                     {
-                        A.Elem[i, i] = 1;
-                        xTrue.Elem[i] = 1;
-                        for (int j = i + 1; j < size; j++)
-                        {
-                            A.Elem[i, j] = 0.01 * (i + j + 1);
-                            A.Elem[j, i] = 0.02 * (i + j + 1);
-                        }
+                        F.Elem[i] = device.NextDouble();
+                        for (int j = 0; j < size; j++)
+                            A.Elem[i, j] = device.NextDouble();
                     }
 
-                    Vector F = A * xTrue;
+                    //Vector xTrue = new Vector(size);
+                    //for (int i = 0; i < size; i++)
+                    //{
+                    //    A.Elem[i, i] = 1;
+                    //    xTrue.Elem[i] = 1;
+                    //    for (int j = i + 1; j < size; j++)
+                    //    {
+                    //        A.Elem[i, j] = 0.01 * (i + j + 1);
+                    //        A.Elem[j, i] = 0.02 * (i + j + 1);
+                    //    }
+                    //}
+                    //Vector F = A * xTrue;
+
                     //Vector X = Gauss.StartSolver(A, F);
                     //Vector X = Givens.StartSolverQR(A, F);
                     //Vector X = GramSchmidt.StartSolverQR(A, F);
