480b36839647516ce8acd6e4ffddff9196730b6e
takaiten <lopilopj@gmail.com>
2019-09-30 00:56:00 +0700

    Make all measurements run at once

--- NumericalAnalysis/Program.cs
+++ NumericalAnalysis/Program.cs
@@ -10,7 +10,7 @@ namespace ComMethods
         {
             try
             {
-                const int size = 500;
+                const int size = 300;
                 Matrix A = new Matrix(size, size);
 
                 Vector F = new Vector(size);
@@ -22,25 +22,43 @@ namespace ComMethods
                         A.Elem[i, j] = device.NextDouble();
                 }
                 
-                var T = new Thread(() =>
+                LUDecomposition lu = new LUDecomposition();
+                var LU = new Thread(() =>
                 {
-                    
-                    //LUDecomposition lu = new LUDecomposition();
-                    //Vector X = lu.StartSolver(A, F);
-
-                    //Vector X = Gauss.StartSolver(A, F);
-
-                    //Vector X = Givens.StartSolverQR(A, F);
-                    //Vector X = GramSchmidt.StartSolverQR(A, F);
-                    Vector X = Householder.StartSolverQR(A, F);
-                    
+                    Vector X = lu.StartSolver(A, F);
                 });
-                // TODO:
-                // Complete this part ???
-                // And think better idea is to measure time in the unit tests
-                // And for some reason Gauss is not working in this test with size bigger than 40
                 
-                Console.WriteLine(CONST.MeasureTime(T));
+                var GAUSS = new Thread(() =>
+                {
+                    Vector X = Gauss.StartSolver(A, F);
+                });
+                
+                var ClassicGS = new Thread(() =>
+                {
+                    Vector X = GramSchmidt.StartClassicSolverQR(A, F);
+                });
+
+                var ModifiedGS = new Thread(() =>
+                {
+                    Vector X = GramSchmidt.StartModifiedSolverQR(A, F);
+                });
+
+                var GIVENS = new Thread(() =>
+                {
+                    Vector X = Givens.StartSolverQR(A, F);
+                });
+
+                var HOUSEHOLDER = new Thread(() =>
+                {
+                    Vector X = Householder.StartSolverQR(A, F);
+                });
+                
+                Console.WriteLine("LU Decomposition " + CONST.MeasureTime(LU));
+                Console.WriteLine("Gauss " + CONST.MeasureTime(GAUSS));
+                Console.WriteLine("Classic Gram-Schmidt " + CONST.MeasureTime(ClassicGS));
+                Console.WriteLine("Modified Gram-Schmidt " + CONST.MeasureTime(ModifiedGS));
+                Console.WriteLine("Givens " + CONST.MeasureTime(GIVENS));
+                Console.WriteLine("Householder " + CONST.MeasureTime(HOUSEHOLDER));
             }
             catch (Exception e)
             {
