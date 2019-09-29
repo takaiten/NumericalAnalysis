7585164eae3bc69479a84fc2f438c799111efc0f
Hexet <49233262+Hexet@users.noreply.github.com>
2019-09-30 01:02:38 +0800

    added changes from branch "newBranch"

--- NumericalAnalysis/Program.cs
+++ NumericalAnalysis/Program.cs
@@ -10,27 +10,30 @@ namespace ComMethods
         {
             try
             {
+                const int size = 500;
+                Matrix A = new Matrix(size, size);
+
+                Vector F = new Vector(size);
+                Random device = new Random();
+                for (int i = 0; i < size; i++)
+                {
+                    F.Elem[i] = device.NextDouble();
+                    for (int j = 0; j < size; j++)
+                        A.Elem[i, j] = device.NextDouble();
+                }
+                
                 var T = new Thread(() =>
                 {
-                    const int size = 20;
-                    Matrix A = new Matrix(size, size);
-                    Vector xTrue = new Vector(size);
-                    for (int i = 0; i < size; i++)
-                    {
-                        A.Elem[i, i] = 1;
-                        xTrue.Elem[i] = 1;
-                        for (int j = i + 1; j < size; j++)
-                        {
-                            A.Elem[i, j] = 0.01 * (i + j + 1);
-                            A.Elem[j, i] = 0.02 * (i + j + 1);
-                        }
-                    }
+                    
+                    //LUDecomposition lu = new LUDecomposition();
+                    //Vector X = lu.StartSolver(A, F);
 
-                    Vector F = A * xTrue;
                     //Vector X = Gauss.StartSolver(A, F);
+
                     //Vector X = Givens.StartSolverQR(A, F);
                     //Vector X = GramSchmidt.StartSolverQR(A, F);
                     Vector X = Householder.StartSolverQR(A, F);
+                    
                 });
                 // TODO:
                 // Complete this part ???
